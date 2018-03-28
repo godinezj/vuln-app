ecd70787f0b1633f954e5a698ef37e90cb64556b
Javier Godinez <godinezj@gmail.com>
2018-03-27 20:30:46 -0700

    Initial commit

---
+++ src/main/java/appsec/sd/RandomTest.java
@@ -0,0 +1,12 @@
+package appsec.sd;
+
+import java.util.Random;
+
+public class RandomTest {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		System.out.println(r.nextInt());
+	}
+
+}
